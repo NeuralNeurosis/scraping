bdfc1df4782754a456137d906217b37f24fa56b7
Dane <neuralneurosis@gmail.com>
2018-10-16 13:44:26 -0400

    tried using the scrape2csv againn not successful.

---
+++ scrape2csv.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var request = require('request');
+var cheerio = require('cheerio');
+var scrape2csv = require('scrape2csv');
+var fs = require('fs');
+var port = 8080;
+
+//let's scrape a very cool website
+var url_to_scrape = "https://www.weddingwire.com/c/az-arizona/lighting-decor/24-sca.html";
+
+var jquery_selector = ".state";
+
+//each article of the page will go through this
+var handler = function($, elem, index){
+	var vendor_state= $(elem).find("a.dark strong").text();
+	var state_url = $(elem).find("a.dark strong").attr("href");
+
+	//returning a new row for the csv
+	return [index,vendor_state,"http://www.weddingwire.com"+state_url];
+}
+
+//optional CSV header
+var header = ["#", "Title of the article", "URL"];
+
+scrape2csv.scrape("/echojs.csv", url_to_scrape, jquery_selector, handler, header);
