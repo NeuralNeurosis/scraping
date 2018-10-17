f1f02fe9a6f76f8325019c08044c4472d644fe6d
Dane <neuralneurosis@gmail.com>
2018-10-17 15:19:45 -0400

    Revert "tired adding in the array of vendor pages i needed to scrape but it is returning nothing"
    This reverts commit c1b8c5646ec8c34b5dbcb2c54613c55b7ca1145f.

--- promises.js
+++ promises.js
@@ -2,72 +2,11 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs-extra');
 
-var baseURI ="https://www.weddingwire.com/lighting-decor?page=43"
-
-var baseUriArray =["https://www.weddingwire.com/lighting-decor?page=43",
-"https://www.weddingwire.com/lighting-decor?page=44",
-"https://www.weddingwire.com/lighting-decor?page=45",
-"https://www.weddingwire.com/lighting-decor?page=46",
-"https://www.weddingwire.com/lighting-decor?page=47",
-"https://www.weddingwire.com/lighting-decor?page=48",
-"https://www.weddingwire.com/lighting-decor?page=49",
-"https://www.weddingwire.com/lighting-decor?page=50",
-"https://www.weddingwire.com/lighting-decor?page=51",
-"https://www.weddingwire.com/lighting-decor?page=52",
-"https://www.weddingwire.com/lighting-decor?page=53",
-"https://www.weddingwire.com/lighting-decor?page=54",
-"https://www.weddingwire.com/lighting-decor?page=55",
-"https://www.weddingwire.com/lighting-decor?page=56",
-"https://www.weddingwire.com/lighting-decor?page=57",
-"https://www.weddingwire.com/lighting-decor?page=58",
-"https://www.weddingwire.com/lighting-decor?page=59",
-"https://www.weddingwire.com/lighting-decor?page=60",
-"https://www.weddingwire.com/lighting-decor?page=61",
-"https://www.weddingwire.com/lighting-decor?page=62",
-"https://www.weddingwire.com/lighting-decor?page=63",
-"https://www.weddingwire.com/lighting-decor?page=64",
-"https://www.weddingwire.com/lighting-decor?page=65",
-"https://www.weddingwire.com/lighting-decor?page=66",
-"https://www.weddingwire.com/lighting-decor?page=67",
-"https://www.weddingwire.com/lighting-decor?page=68",
-"https://www.weddingwire.com/lighting-decor?page=69",
-"https://www.weddingwire.com/lighting-decor?page=70",
-"https://www.weddingwire.com/lighting-decor?page=71",
-"https://www.weddingwire.com/lighting-decor?page=72",
-"https://www.weddingwire.com/lighting-decor?page=73",
-"https://www.weddingwire.com/lighting-decor?page=74",
-"https://www.weddingwire.com/lighting-decor?page=75",
-"https://www.weddingwire.com/lighting-decor?page=76",
-"https://www.weddingwire.com/lighting-decor?page=77",
-"https://www.weddingwire.com/lighting-decor?page=78",
-"https://www.weddingwire.com/lighting-decor?page=79",
-"https://www.weddingwire.com/lighting-decor?page=80",
-"https://www.weddingwire.com/lighting-decor?page=81",
-"https://www.weddingwire.com/lighting-decor?page=82",
-"https://www.weddingwire.com/lighting-decor?page=86",
-"https://www.weddingwire.com/lighting-decor?page=83",
-"https://www.weddingwire.com/lighting-decor?page=84",
-"https://www.weddingwire.com/lighting-decor?page=85",
-"https://www.weddingwire.com/lighting-decor?page=87",
-"https://www.weddingwire.com/lighting-decor?page=88",
-"https://www.weddingwire.com/lighting-decor?page=89",
-"https://www.weddingwire.com/lighting-decor?page=90",
-"https://www.weddingwire.com/lighting-decor?page=91",
-"https://www.weddingwire.com/lighting-decor?page=92",
-"https://www.weddingwire.com/lighting-decor?page=93",
-"https://www.weddingwire.com/lighting-decor?page=94",
-"https://www.weddingwire.com/lighting-decor?page=95",
-"https://www.weddingwire.com/lighting-decor?page=96",
-"https://www.weddingwire.com/lighting-decor?page=97",
-"https://www.weddingwire.com/lighting-decor?page=98",
-"https://www.weddingwire.com/lighting-decor?page=99",
-"https://www.weddingwire.com/lighting-decor?page=100",
-"https://www.weddingwire.com/lighting-decor?page=101"
-]
+var baseURI =" https://www.weddingwire.com/lighting-decor"
 
 getVendorContactAsync()
 .then(function(data){
-    fs.writeFile('./returned data/promises/vendorcontact44-101.json', JSON.stringify(data, null, 4), (err)=>{
+    fs.writeFile('./vendorcontact.json', JSON.stringify(data, null, 4), (err)=>{
         if (err) {
             console.error(err);
             return;
@@ -83,14 +22,8 @@ getVendorContactAsync()
 
 
 function getVendorEndPointsAsync(){
-     new Promise(function(resolve, reject){
-        
-        
-        var promises = baseUriArray.map(function (ends){
-        
-            return new Promise(function (resolve, reject){
-
-        request(ends, function(error, response, body){
+    return new Promise(function(resolve, reject){
+        request(baseURI, function(error, response, body){
 
             if (!error){
  
@@ -108,18 +41,7 @@ function getVendorEndPointsAsync(){
                 reject('SHITTTTTTT ');
             }
       });
-    });
-});
-return Promise.all(promises);
-
-})
-.then(function (data1){
-    console.log(data1);
-})
-.catch(function (err){
-    return Promise.reject(err);
-});
-
+  });
 }
 
 function getVendorContactAsync(){
